a36a0acbbe06db5d8ecd38d940b68e568f50e1db
Naitik Jain <60510325+naitikjain19@users.noreply.github.com>
2022-03-25 02:10:36 +0530

    Update settings.py

--- Disposal_Recycle_System/Vyarth/bvphack/vyarth/settings.py
+++ Disposal_Recycle_System/Vyarth/bvphack/vyarth/settings.py
@@ -20,7 +20,7 @@ TEMPLATE_DIR= os.path.join(BASE_DIR,"templates")
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '12345+&yn!2vg7-=@9g1ru$z(hu85r6*4q7n*72#$)l!6i7890')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
