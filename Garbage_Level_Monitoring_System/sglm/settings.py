51bad08ccf8e02c06182ca5478f5441d0832b9f4
Naitik Jain <60510325+naitikjain19@users.noreply.github.com>
2022-03-25 02:14:52 +0530

    Update settings.py

--- Garbage_Level_Monitoring_System/sglm/settings.py
+++ Garbage_Level_Monitoring_System/sglm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '(4!w2s$spv()37or!5ebk&5*5sgu20u!ujudd3gdcpt$dcefp!')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECRET_KEY = '(4!w2s$spv()37or!5ebk&5*5sgu20u!ujudd3gdcpt$dcefp!'
 
